e7ec8ab0e3296ecdddc96fbdd46d0cce205c6787
Giovanni Smith <51383214+GiovanniSmith@users.noreply.github.com>
2023-12-08 17:34:47 -0600

    Removed ChooseWord.java and moved contents to WordDisplay.java

--- src/GameController.java
+++ src/GameController.java
@@ -1,7 +1,6 @@
 // This class would handle game logic
 // This class would handle what word the player has to guess, checking if the guess matches the word
 
-
 public class GameController {
     
 }
--- src/WordDisplay.java
+++ src/WordDisplay.java
@@ -4,20 +4,35 @@
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
 
 public class WordDisplay extends HBox {
+    String[] words = {"cake", "bird", "tree", "play", "frog", "book", "rain", "fish", 
+    "game", "ship", "apple", "beach", "chair", "dance", "eagle", "fancy", "glass", 
+    "happy", "igloo", "jelly", "banana", "camera", "dancer", "eleven", "falcon", 
+    "guitar", "hammer", "jacket", "killer", "laptop"};
+
+    String wordToGuess;
+    Rectangle blank;
 
 
     public WordDisplay() {
-        this.setSpacing(5); 
-        this.setPadding(new Insets(20)); 
+        this.setSpacing(5);
+        this.setPadding(new Insets(20));
 
         for (char c = 'A'; c <= 'Z'; c++) {
             Button letterButton = new Button(String.valueOf(c));
-            letterButton.setPrefWidth(40); 
-            letterButton.setPrefHeight(40); 
-            this.getChildren().add(letterButton); 
+            letterButton.setPrefWidth(40);
+            letterButton.setPrefHeight(40);
+            this.getChildren().add(letterButton);
         }
+
+        
     }
 
+    public String getRandomWord(){
+        String randomWord = words[(int) Math.floor(Math.random() * words.length)];
+        String word = randomWord;
+        return word;
+    }
 }
